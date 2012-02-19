1b2187535ebeee78614ec2ce41698554b8b3cbb0
Hendrik Mans <hendrik@mans.de>
2012-02-19 04:11:38 +0100

    Add gaug.es and twitter

--- app.rb
+++ app.rb
@@ -21,8 +21,8 @@ class App < SchnitzelPress::App
     #
     # set :disqus_name, "..."
     # set :google_analytics_id, "..."
-    # set :gauges_id, "..."
-    # set :twitter_id, '...'
+    set :gauges_id, "4f40682a844d524b41000068"
+    set :twitter_id, 'teamschnitzel'
     # set :read_more, "Read ALL the things"
   end
 end
